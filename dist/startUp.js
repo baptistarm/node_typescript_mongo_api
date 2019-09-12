f8c6c82bb1deb778840fdf273f960bfd6bcdfe86
Rodrigo Baptista <rodrigombaptista@gmail.com>
2019-09-11 23:19:55 -0300

    Connecting startUp class to database

--- dist/startUp.js
+++ dist/startUp.js
@@ -1,9 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const database_1 = require("./infra/database");
 class StartUp {
     constructor() {
         this.app = express();
+        this._db = new database_1.default();
+        this._db.createConnection();
         this.routes();
     }
     routes() {
